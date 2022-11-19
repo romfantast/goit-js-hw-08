3f341bbd1297c6979c49a954f9aacb597f6eeadd
romfantast <romfantast@gmail.com>
2022-11-20 00:48:20 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,24 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-const formEl = document.querySelector(".feedback-form");
+const formEl = document.querySelector('.feedback-form');
 const refs = {
-  email: document.querySelector("input"),
-  message: document.querySelector("textarea"),
+  email: document.querySelector('input'),
+  message: document.querySelector('textarea'),
 };
 
-const FORM_STATE = "feedback-form-state";
+const FORM_STATE = 'feedback-form-state';
 
 if (localStorage.getItem(FORM_STATE)) {
-  refs.email.value = JSON.parse(localStorage.getItem(FORM_STATE)).email;
-  refs.message.value = JSON.parse(localStorage.getItem(FORM_STATE)).message;
-} else {
-  refs.email.value = "";
-  refs.message.value = "";
+  refs.email.value = JSON.parse(localStorage.getItem(FORM_STATE)).email || '';
+  refs.message.value =
+    JSON.parse(localStorage.getItem(FORM_STATE)).message || '';
 }
 
 let result = {};
 
-formEl.addEventListener("input", throttle(handleFormInput, 500));
+formEl.addEventListener('input', throttle(handleFormInput, 500));
 
-formEl.addEventListener("submit", handlerFormSubmit);
+formEl.addEventListener('submit', handlerFormSubmit);
 
 function handleFormInput(e) {
   result[e.target.name] = e.target.value;
