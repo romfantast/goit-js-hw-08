a903d90bfecdc0647e1eefda7a881f2ff988e9d9
romfantast <romfantast@gmail.com>
2022-11-20 21:50:07 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,30 +1,30 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 // Change code below this line
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const galleryList = document.querySelector(".gallery");
+const galleryList = document.querySelector('.gallery');
 
 function createGalleryMarkup(gallery) {
   return gallery
-    .map((photo) => {
+    .map(photo => {
       return `
 			<a class="gallery__item" href="${photo.original}">
   <img class="gallery__image" src="${photo.preview}" alt="${photo.description}" />
 </a>
 		`;
     })
-    .join("");
+    .join('');
 }
 
 galleryList.innerHTML = createGalleryMarkup(galleryItems);
 
-new SimpleLightbox(".gallery a", {
+new SimpleLightbox('.gallery a', {
   alertError: true,
-  captionsData: "alt",
-  captionPosition: "bottom",
+  captionsData: 'alt',
+  captionPosition: 'bottom',
   captionDelay: 300,
   animationSpeed: 150,
   fadeSpeed: 150,
